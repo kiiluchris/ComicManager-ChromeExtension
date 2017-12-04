d5b9b5b7a9d9bf683bb992049110486de7edd57e
kiilu_chris <cndeti@gmail.com>
2017-12-04 19:49:49 +0300

    filter items in JSON to depth of one while merging objects

--- src/options/index.js
+++ src/options/index.js
@@ -1,12 +1,21 @@
-import {importData, exportData} from './file';
+import {importData, exportData, mergeJSONObjects} from './file';
 
 (() => {
-  document.querySelector('button#import')
+  const cleanupKeys = ['webtoonOrder', 'novels'];
+  const filterKeys = {
+    webtoonOrder: 'link'
+  }
+  const seededKeys = ['webtoonOrder'];
+  document.querySelector('main button#import')
     .addEventListener('click', () => {
-      importData().catch(console.error);
+      importData({cleanupKeys, filterKeys, seededKeys}).catch(console.error);
     });
-  document.querySelector('button#export')
+  document.querySelector('main button#export')
     .addEventListener('click', () => {
       exportData().catch(console.error);
+    });
+  document.querySelector('main button#merge')
+    .addEventListener('click', () => {
+      importData({cleanupKeys, filterKeys, seededKeys}, false).catch(console.error);
     });
 })();
