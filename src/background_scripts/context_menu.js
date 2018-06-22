f1a9504d17310c1821712af8de78e78e9a2b531b
kiilu_chris <cndeti@gmail.com>
2018-06-22 08:05:01 +0300

    update webtoon fave url

--- src/background_scripts/context_menu.js
+++ src/background_scripts/context_menu.js
@@ -43,7 +43,7 @@ const callbacks = {
   }
 }
 const webtoonFavPattern = [
-  "*://*.webtoons.com/favorite"
+  "*://*.webtoons.com/en/favorite"
 ];
 const kissmangaAllPattern = [
   "*://kissmanga.com/*"
